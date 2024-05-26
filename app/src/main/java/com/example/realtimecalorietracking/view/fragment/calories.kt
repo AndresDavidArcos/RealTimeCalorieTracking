1ec5ce6bfc6cf5cdd55632a2e67bf08fdb3a63d6
Juan Esteban Mazuera Yunda 2043008 <juan.yunda@correounivalle.edu.co>
2024-05-26 16:03:35 -0500

    Mostrar cardview de los registros de alimentos, sus calorias y fechas

--- app/src/main/java/com/example/realtimecalorietracking/view/fragment/calories.kt
+++ app/src/main/java/com/example/realtimecalorietracking/view/fragment/calories.kt
@@ -8,17 +8,34 @@ import android.view.ViewGroup
 import com.example.realtimecalorietracking.R
 import com.example.realtimecalorietracking.databinding.FragmentCaloriesBinding
 import com.example.realtimecalorietracking.databinding.FragmentHomeBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.realtimecalorietracking.view.adapter.CaloriesItem
+import com.example.realtimecalorietracking.view.adapter.CaloriesAdapter
 
 
 class calories : Fragment() {
 
     private lateinit var binding: FragmentCaloriesBinding
+    private lateinit var adapter: CaloriesAdapter
+    private lateinit var calorieItemList: MutableList<CaloriesItem>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentCaloriesBinding.inflate(inflater)
+
+        // Initialize data
+        calorieItemList = mutableListOf(
+            CaloriesItem("Ensalada", "200 Cal", "25-05-2024"),
+            CaloriesItem("Pizza", "400 Cal", "24-05-2024")
+            // Add more items as needed
+        )
+
+        adapter = CaloriesAdapter(calorieItemList)
+        binding.recyclerViewCalories.layoutManager = LinearLayoutManager(context)
+        binding.recyclerViewCalories.adapter = adapter
+
         return binding.root
     }
 

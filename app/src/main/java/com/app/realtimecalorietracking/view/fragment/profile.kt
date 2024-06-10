efb404a6ab18789b26336c278688170df0fbc901
Juan Esteban Mazuera Yunda 2043008 <juan.yunda@correounivalle.edu.co>
2024-06-10 18:06:02 -0500

    Ajustar RecyclerView para llenar la pantalla, aumentar el tamaño del texto de CardView, eliminar la barra de herramientas por defecto.

--- app/src/main/java/com/app/realtimecalorietracking/view/fragment/profile.kt
+++ app/src/main/java/com/app/realtimecalorietracking/view/fragment/profile.kt
@@ -1,25 +1,56 @@
 package com.app.realtimecalorietracking.view.fragment
 
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import com.app.realtimecalorietracking.MainActivity
 import com.app.realtimecalorietracking.R
 import com.app.realtimecalorietracking.databinding.FragmentHomeBinding
 import com.app.realtimecalorietracking.databinding.FragmentProfileBinding
+import com.app.realtimecalorietracking.view.LoginActivity
+import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class profile : Fragment() {
     private lateinit var binding: FragmentProfileBinding
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentProfileBinding.inflate(inflater)
+        sharedPreferences = requireActivity().getSharedPreferences("shared", Context.MODE_PRIVATE)
+        controladores()
         return binding.root
+    }
+
+    private fun logOut() {
+        sharedPreferences.edit().clear().apply()
+        FirebaseAuth.getInstance().signOut()
+        (requireActivity() as MainActivity).apply {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun controladores() {
+        val btnLogOut: ImageView = binding.root.findViewById(R.id.btnLogOut)
+        btnLogOut.setOnClickListener {
+            it.animate().scaleX(0.8f).scaleY(0.8f).setDuration(200).withEndAction {
+                it.animate().scaleX(1f).scaleY(1f).setDuration(200).start()
+                //activity?.onBackPressedDispatcher?.onBackPressed()
+                logOut()
+            }.start()
+        }
+
     }
 
 }
